6d3b31a48c217871f9f25f1b46f80162682a7b3c
Vadym Tymchenko <vtymchenko@uccon.org>
2015-04-23 17:08:04 -0700

    Fixed the issue with the image. Now it should also work slightly faster

--- DiabeticRetinopathy/autocrop.py
+++ DiabeticRetinopathy/autocrop.py
@@ -2,12 +2,14 @@ import numpy as np
 from skimage.util import crop
 
 def crop_manual(img):
+    threshold = 20000
     s = np.sum(img, axis=2)
-    cols = np.sum(s, axis=0) 
-    rows = np.sum(s, axis=1)
-    left_border = np.argmax(cols[0:len(cols)/2] > 10000)
-    right_border = len(cols)/2 - np.argmax(cols[len(cols)/2:len(cols)-1] < 10000)
-    upper_border = np.argmax(rows[0:len(rows)/2] > 10000)
-    lower_border = len(rows)/2 - np.argmax(rows[len(rows)/2:len(rows)-1] < 10000)
+    cols = np.sum(s, axis=0) > threshold  
+    rows = np.sum(s, axis=1) > threshold
     
-    return crop(img, ((upper_border, lower_border),(left_border, right_border),  (0,0)))
+    left_border = np.argmax(cols[0:len(cols)/2])
+    right_border = np.argmax(cols[len(cols)-1:len(cols)/2:-1])
+    upper_border = np.argmax(rows[0:len(rows)/2])
+    lower_border = np.argmax(rows[len(rows)-1:len(rows)/2:-1])
+  
+    return skimage.util.crop(img, ((upper_border, lower_border),(left_border, right_border),  (0,0)))
