86b0afdff54029bca2ea72f7a3fa809d7ec5486f
fierval <fierval@gmail.com>
2015-05-27 12:27:08 -0800

    Split files for digits

--- DiabeticRetinopathy/Learning/get_sample.py
+++ DiabeticRetinopathy/Learning/get_sample.py
@@ -9,6 +9,7 @@ from skimage.io.collection import ImageCollection
 import os
 from os import path
 from shutil import copy, rmtree
+from tr_utils import prep_out_path
 
 def get_sample_csv(sample_size = 0.1):
     X, Y, _, _ = TrainFiles.from_csv(inp_file, test_size = sample_size)
@@ -44,4 +45,24 @@ def sample_files(labels, size, inp_path = raw_train, out_path = sample_train):
     out_files = [path.join(out_path, f + ".jpeg") for f in picked_files]
     for src, dst in zip(inp_files, out_files):
         copy(src, dst)
+
+labels_file = "/home/boris/Kaggle/Retina/trainLabels.csv"    
+inp_path = "/home/boris/Dropbox/Kaggle/retina/train/thresh5size512"
+out_path = "/home/boris/Dropbox/Kaggle/retina/train/digits"
+
+def copy_files_to_label_dirs(inp_path, out_path, labels_file):
+    prep_out_path(out_path)
+    
+    for i in range(0, 5):
+        os.makedirs(path.join(out_path, str(i)))
+    
+    labels = pd.read_csv(labels_file)
+    existing_files = pd.DataFrame([path.splitext(f)[0] for f in os.listdir(inp_path)], columns =[labels.columns[0]])
+    existing_files = existing_files.merge(labels, on=existing_files.columns[0])
+    
+    for f, l in zip(existing_files['image'], existing_files['level']):
+        file_name = path.join(out_path, str(l), f + ".jpeg")
+        inp_file = path.join(inp_path, f + '.jpeg')
+        copy(inp_file, file_name)
+        print "copied {0} to {1}".format(inp_file, file_name)
     
