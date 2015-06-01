8f03920732b86710095141087ed2d20adf872b40
fierval <fierval@gmail.com>
2015-05-31 20:44:28 -0700

    Adding "imaging"

---
+++ Refactoring/kobra/imaging.py
@@ -0,0 +1,38 @@
+from matplotlib import pyplot as plt
+from os import path
+import numpy as np
+import cv2
+import pandas as pd
+
+
+def show_images(images,titles=None, scale=1.3):
+    """Display a list of images"""
+    n_ims = len(images)
+    if titles is None: titles = ['(%d)' % i for i in range(1,n_ims + 1)]
+    fig = plt.figure()
+    n = 1
+    for image,title in zip(images,titles):
+        a = fig.add_subplot(1,n_ims,n) # Make subplot
+        if image.ndim == 2: # Is image grayscale?
+            plt.imshow(image)
+        else:
+            plt.imshow(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+        a.set_title(title)
+        plt.axis("off")
+        n += 1
+    fig.set_size_inches(np.array(fig.get_size_inches(), dtype=np.float) * n_ims / scale)
+    plt.show()
+    
+# Pyramid Down & blurr
+# Easy-peesy
+def pyr_blurr(image):
+    return cv2.GaussianBlur(cv2.pyrDown(image), (7, 7), 30.)
+
+def median_blurr(image, size = 7):
+    return cv2.medianBlur(image, size)
+
+def display_contours(image, contours, color = (255, 0, 0), thickness = -1, title = None):
+    imShow = image.copy()
+    for i in range(0, len(contours)):
+        cv2.drawContours(imShow, contours, i, color, thickness)
+    show_images([imShow], scale=0.7, titles=title)
