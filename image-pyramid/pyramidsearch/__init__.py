b444ad3faa94134e15c9a0a3f1b513e6caad6391
fierval <fierval@gmail.com>
2015-06-08 20:57:55 -0700

    image-pyramid related package

---
+++ image-pyramid/pyramidsearch/__init__.py
@@ -0,0 +1,1 @@
+from helpers import sliding_window, pyramid
---
+++ image-pyramid/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyimagesearch',
+      version='0.1',
+      description='Pyramid Search',
+      url='Adrian Rosebrock',
+      author='Adrian Rosebrock',
+      author_email='a.rosebrock@pyimagesearch.com',
+      license='MIT',
+      packages=['pyramidsearch'],
+      zip_safe=False)
