b9a1dc395f61503b6e7194a5d93135fdebe20b33
fierval <fierval@gmail.com>
2015-06-16 15:42:28 -0700

    Removing junk

--- DiabeticRetinopathy/Refactoring/build/lib/kobra/__init__.py
+++
@@ -1,8 +0,0 @@
-from train_files import TrainFiles
-from SupervisedLearning import SKSupervisedLearning
-from od_detection import DetectOD
-from image_reader import ImageReader
-import cv2
-
-def enum(**enums):
-    return type('Enum', (), enums)
