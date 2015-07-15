260b49523d9f4dfcf37c94a3fe71b7357c9f4828
fierval <fierval@gmail.com>
2015-07-15 11:55:49 -0700

    Boosting experiments

--- DiabeticRetinopathy/Learning/learn_boost.py
+++ DiabeticRetinopathy/Learning/learn_boost.py
@@ -1,10 +1,11 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import train_test_split
-from kobra import SKSupervisedLearning
 from kobra.tr_utils import time_now_str
 import numpy as np
+import sklearn.preprocessing as prep
+from sklearn import metrics
 
 sample_file = '/kaggle/retina/reduced/features/sample/features.csv'
 
@@ -19,26 +20,21 @@ X_train, X_test, Y_train, Y_test = train_test_split(feats, levels, test_size = 0
 
 print "Read, train: {:d}, test: {:d}".format(X_train.shape[0], X_test.shape[0])
 
-rf = SKSupervisedLearning(SVC, X_train, Y_train, X_test, Y_test)
-#rf.estimation_params = {'max_depth' : [4, 10, 100], 'min_samples_leaf': [3, 5, 20], 
-#                         'max_features': [1.0, 0.3, 0.1]}
+inner_estimator = DecisionTreeClassifier(max_depth = 15, class_weight = 'auto')
+abc = AdaBoostClassifier(base_estimator = inner_estimator, n_estimators = 175)
 
-# parameters tuned from the above
-#rf.train_params = {'n_estimators' : 1000, 'max_features': 'sqrt'}
-rf.train_params = {'C': 100, 'gamma' : 0.001, 'probability' : True, 'class_weight': 'auto'}
-
-rf.scoring = "accuracy"
 print "Instantiated classifier"
-
-rf.fit_standard_scaler()
-#rf.grid_search_classifier()
-
 print "Starting: ", time_now_str()
 
-a_train, a_test = rf.fit_and_validate()
+#scaler = prep.StandardScaler().fit(X_train)
+#X_train_scaled = scaler.transform(X_train)
+#X_test_scaled = scaler.transform(X_test)
+
+abc.fit(X_train, Y_train)
+a_train = metrics.accuracy_score(Y_train, abc.predict(X_train))
+a_test = metrics.accuracy_score(Y_test, abc.predict(X_test))
 
 print "Finished: ", time_now_str()
 
 print "Accuracy: \n\tTrain: {:2.5f}\n\tTest: {:2.5f}".format(a_train, a_test)
 
-rf.plot_confusion()
--- DiabeticRetinopathy/Learning/predict.py
+++ DiabeticRetinopathy/Learning/predict.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC, LinearSVC
 from sklearn.cross_validation import train_test_split
 from kobra import SKSupervisedLearning
 from kobra.tr_utils import time_now_str
@@ -25,18 +26,20 @@ images = df_test['name'].values
 
 print "Read, train: {:d}, test: {:d}".format(X_train.shape[0], X_test.shape[0])
 
-rf = SKSupervisedLearning(RandomForestClassifier, X_train, Y_train, X_test, Y_test)
+rf = SKSupervisedLearning(SVC, X_train, Y_train, X_test, Y_test)
 
 # parameters tuned from the above
-rf.train_params = {'n_estimators' : 1000, 'max_features': 'sqrt'}
+#rf.train_params = {'n_estimators' : 1000, 'max_features': 'sqrt', 'class_weight': 'auto'}
+rf.train_params = {'C': 100, 'gamma' : 0.001, 'probability' : True, 'class_weight': 'auto'}
 rf.scoring = "accuracy"
 print "Instantiated classifier"
 
 print "Starting: ", time_now_str()
 
+rf.fit_standard_scaler()
 rf.fit_and_validate()
 Y_test = rf.clf.predict(X_test)
 pred = pd.DataFrame(np.array([images, Y_test]).transpose(), columns = ['image', 'level'])
-pred.to_csv(path.join(pred_dir, "prediction.csv"), index = False)
+pred.to_csv(path.join(pred_dir, "prediction2.csv"), index = False)
 
 print "Finished: ", time_now_str()
